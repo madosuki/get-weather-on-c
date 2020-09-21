c287dd3014cc9876bf1ac3f9e90e48725381a687
madosuki <yurikasamaaikatsu@gmail.com>
2020-09-21 16:28:26 +0900

    add separate body and header from response data function

--- main.c
+++ main.c
@@ -23,6 +23,12 @@
 #define TRUE 1
 #define FALSE 0
 
+#define FREE(v) \
+  do {          \
+    free(v);\
+    v = NULL;\
+  } while(0);
+
 #define INIT_ARRAY(type, size) (type*)calloc(size, sizeof(type))
 
 typedef enum {
@@ -52,6 +58,96 @@ typedef struct UrlDataStruct {
   int protocol;
 } url_data_s;
 
+typedef struct ResponseStruct
+{
+  char *header;
+  char *body;
+  ssize_t header_size;
+  ssize_t body_size;
+} response_s;
+
+int set_http_response_data(const char *response_data, ssize_t size, response_s *result)
+{
+  if(response_data == NULL) {
+    printf("Error: response data is NULL\n");
+    return -1;
+  }
+
+  if(size < 1) {
+    printf("Error: size is less than one.\n");
+    return -1;
+  }
+
+  char *header = INIT_ARRAY(char, size);
+  ssize_t count = 0;
+  int new_line_count = 0;
+  char previous = 0;
+  for(ssize_t i = 0; i < size; ++i) {
+
+    if(previous == '\n' && response_data[i] != '\r') {
+      new_line_count = 0;
+      previous = 0;
+    }
+
+    if(previous == '\r') {
+      if(response_data[i] == '\n') {
+        ++new_line_count;
+        previous = '\n';
+      } else {
+        new_line_count = 0;
+        previous = 0;
+      }
+    }
+
+    if(response_data[i] == '\r') {
+      previous = '\r';
+    }
+
+    header[i] = response_data[i];
+    
+    ++count;
+
+    if(new_line_count == 2)
+      break;
+    
+  }
+
+  if(count >= size) {
+    FREE(header);
+
+    printf("Error: missing body\n");
+
+    return -1;
+  }
+
+  char *reallocated = realloc(header, count);
+  if(reallocated == NULL) {
+    FREE(header);
+
+    printf("Error: failed reallocate at header\n");
+
+    return -1;
+  }
+
+  header = reallocated;
+
+  char *body = INIT_ARRAY(char, size);
+  ssize_t body_pos = 0;
+  for(ssize_t i = count; i < size; ++i) {
+    body[body_pos] = response_data[i];
+    ++body_pos;
+  }
+
+  result->body_size = body_pos;
+  result->body = body;
+
+  result->header_size = count;
+  // result->header_size = strlen(header);
+  result->header = header;
+  
+  return 1;
+}
+
 void get_ipaddr_from_host(struct hostent *host, char **list)
 {
   if(host->h_length > 4) {
@@ -80,7 +176,7 @@ void get_ipaddr_from_host(struct hostent *host, char **list)
       char *result = INIT_ARRAY(char, size);
       sprintf(result, "%d.%d.%d.%d", byte_list[0], byte_list[1], byte_list[2], byte_list[3]);
 
-      free(byte_list);
+      FREE(byte_list);
 
       list[i] = result;
 
@@ -136,12 +232,12 @@ void set_addr(socket_data_s *socket_data, const url_data_s *url_data)
 
   for(int i = 0; i < 1024; ++i) {
     if(ip_list[i] != NULL) {
-      free(ip_list[i]);
+      FREE(ip_list[i]);
       ip_list[i] = NULL;
     }
   }
 
-  free(ip_list);
+  FREE(ip_list);
   
 }
 
@@ -212,12 +308,13 @@ int do_connect(socket_data_s *socket_data, int protocol, int is_ssl, const char 
   FD_SET(socket_data->socket, &readfds);
   
   int count = 0;
-  /* char *result = NULL; */
-  /* long result_size = 0; */
+  char *result = NULL;
+  long result_size = 0;
   do{
 
     memcpy(&fds, &readfds, sizeof(fd_set));
 
+    // first argument is must be that socket + 1. This value is max size of socket.
     err = select(socket_data->socket + 1, &fds, NULL, NULL, &tv);
     if(!err) {
       printf("timeout\n");
@@ -229,29 +326,27 @@ int do_connect(socket_data_s *socket_data, int protocol, int is_ssl, const char 
       memset(buf, 0, sizeof(buf));
     
       if(!is_ssl)
-        readed_size = read(socket_data->socket, buf, BUF_SIZE);
+        readed_size = recv(socket_data->socket, buf, BUF_SIZE, 0);
       else
         readed_size = SSL_read(ssl, buf, BUF_SIZE);
 
       printf("count: %d, readed size: %ld\nbody: %s\n", count, readed_size, buf);
     }
 
-    /* result_size += readed_size; */
-    /* if(result == NULL) { */
-    /*   result = INIT_ARRAY(char, result_size); */
-    /*   strncpy(result, buf, readed_size); */
-    /* } else { */
-    /*   char *check_realloc = realloc(result, result_size); */
-    /*   if(check_realloc != NULL) */
-    /*     strcat(result, buf); */
-    /* } */
+    result_size += readed_size;
+    if(result == NULL) {
+      result = INIT_ARRAY(char, result_size);
+      strncpy(result, buf, readed_size);
+    } else {
+      char *check_realloc = realloc(result, result_size);
+      if(check_realloc != NULL)
+        strcat(result, buf);
+    }
 
     ++count;
 
   } while(readed_size > 0 && count < 3);
 
-  // printf("result data: %s\n", result);
-  // free(result);
 
   if(is_ssl) {
     SSL_shutdown(ssl);
@@ -261,6 +356,24 @@ int do_connect(socket_data_s *socket_data, int protocol, int is_ssl, const char 
   }
 
   close(socket_data->socket);
+
+
+  response_s *response = INIT_ARRAY(response_s, sizeof(response_s));
+  set_http_response_data(result, strlen(result), response);
+  
+  /* printf("\nresult data: %s\n", result); */
+  FREE(result);
+
+  printf("header size: %ld\nheader: %s\n\nbody size: %ld\nbody: %s\n",
+         response->header_size,
+         response->header,
+         response->body_size,
+         response->body);
+
+  FREE(response->header);
+  FREE(response->body);
+  FREE(response);
+
 
   return 1;
 }
@@ -328,7 +441,7 @@ char* create_header(url_data_s *url_data, Method method, HttpVersion version)
   else if(method == POST)
     strncpy(method_string, "POST", 4);
   else {
-    free(method_string);
+    FREE(method_string);
 
     printf("unknown method!\n");
 
@@ -340,8 +453,8 @@ char* create_header(url_data_s *url_data, Method method, HttpVersion version)
     const char *http_1_1 = "HTTP/1.1";
     strncpy(http_version_string, http_1_1, strlen(http_1_1));
   } else {
-    free(method_string);
-    free(http_version_string);
+    FREE(method_string);
+    FREE(http_version_string);
     
     printf("invalid version!\n");
 
@@ -361,8 +474,8 @@ char* create_header(url_data_s *url_data, Method method, HttpVersion version)
   char *header = INIT_ARRAY(char, size);
   sprintf(header, "%s %s/ %s\r\nHost: %s\r\n\r\n", method_string, url_data->path_name, http_version_string, url_data->hostname);
 
-  free(method_string);
-  free(http_version_string);
+  FREE(method_string);
+  FREE(http_version_string);
 
   return header;
 }
@@ -389,16 +502,15 @@ int main(void)
       //  do_connect(&socket_data, HTTP_PORT, FALSE, header);
     }
 
-    free(header);
-    header = NULL;
+    FREE(header);
 
-    free(url_data->hostname);
-    free(url_data->path_name);
-    free(url_data->url);
+    FREE(url_data->hostname);
+    FREE(url_data->path_name);
+    FREE(url_data->url);
 
   }
 
-  free(url_data);
+  FREE(url_data);
 
   return 0;
 }
