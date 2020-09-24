fc477fc1c5ee830c53fa27bbc4143848949ea587
madosuki <yurikasamaaikatsu@gmail.com>
2020-09-24 18:11:28 +0900

    change build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-echo "build start"
 
-cd ./src/lib/http_connector
-make
+# echo "build start"
 
-cd ../
-make
+# cd ./src/lib/http_connector
+# make
+
+# cd ../
+# make
+
+if [ -d ./build ]; then
+    mkdir -p build
+fi
+
+cd build
+
+cmake ..
+
+cmake --build .
 
 echo "done! (^ω^)⊃🍺"
