933d1905e2c9434ef22667df666af3db61fd3b96
madosuki <yurikasamaaikatsu@gmail.com>
2020-09-25 16:20:56 +0900

    add support setting json file

--- src/main/main.c
+++ src/main/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <json-c/json.h>
 
 #include "../lib/openweathermap_client/openweather.h"
 
@@ -16,13 +17,31 @@ int main(int argc, const char **argv)
     return -1;
   }
 
-  openweather_query_s query = {};
-  query.api_key = INIT_ARRAY(char, strlen(argv[1]));
-  strncpy(query.api_key, argv[1], strlen(argv[1]));
+  struct json_object *account = json_object_from_file("./openweather_map_account.json");
+  if(account == NULL) {
+    printf("Error: openweather_map_account.json is not found\n");
+    return -1;
+  }
+  
+  struct json_object *tmp = json_object_object_get(account, "api_key");
+  if(tmp == NULL) {
+    printf("Error: missing api_key in openweather_map_account.json\n");
+    return -1;
+  }
+  const char *api_key = json_object_to_json_string(tmp);
+  ssize_t api_key_size = strlen(api_key);
 
-  query.city_name = "London";
+  openweather_query_s query = {};
+  query.api_key = INIT_ARRAY(char, api_key_size);
+  strncpy(query.api_key, api_key + 1, api_key_size - 2);
+
+  ssize_t city_name_size = strlen(argv[1]);
+  query.city_name = INIT_ARRAY(char, city_name_size);
+  strncpy(query.city_name, argv[1], city_name_size);
 
   openweather_map_current_s *result = get_openweather_map_current_data(&query);
+  FREE(query.api_key);
+  FREE(query.city_name);
   FREE(result);
 
   return 0;
